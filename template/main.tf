21fab2152685dab64a501c8f06c0363bd3f607f3
eggy1005 <eggy1005@hotmail.com>
2021-07-06 16:37:22 +0100

    aaa

--- template/main.tf
+++ template/main.tf
@@ -1,9 +1,9 @@
-terraform{
-    backend "gcs" {
-        bucket = "${google_storage_bucket.tf_state_bucket.name}"
-        prefix = "terraform/state"
-    }
-}
+# terraform{
+#     backend "gcs" {
+#         bucket = "${bucket_name}"
+#         prefix = "terraform/state"
+#     }
+# }
 resource "null_resource" "null" {
     triggers = {
         value = "Doing nothing"
