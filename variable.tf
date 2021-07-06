4561b2631ea5327856979ba30b9322ffa96e96fd
eggy1005 <eggy1005@hotmail.com>
2021-07-06 15:43:35 +0100

    change

--- variable.tf
+++ variable.tf
@@ -5,9 +5,9 @@ variable "github_token" {
 
 variable "github_owner"{
     type = string
-    default = "eggy1005"
+
 }
 variable "gcp_billing_acc"{
     type = string
-    default = "013CB4-BD99C7-67D5F5"
+
 }
