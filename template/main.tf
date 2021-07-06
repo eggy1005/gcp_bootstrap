3eccdd8fb0e18ff6afa79b7ad78be0c0fc32181e
eggy1005 <eggy1005@hotmail.com>
2021-07-06 16:42:08 +0100

    aaa

--- template/main.tf
+++ template/main.tf
@@ -1,9 +1,9 @@
-# terraform{
-#     backend "gcs" {
-#         bucket = "${bucket_name}"
-#         prefix = "terraform/state"
-#     }
-# }
+terraform{
+    backend "gcs" {
+        bucket = "${bucket_name}"
+        prefix = "terraform/state"
+    }
+}
 resource "null_resource" "null" {
     triggers = {
         value = "Doing nothing"
